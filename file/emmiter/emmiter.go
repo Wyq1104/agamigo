ee2aedc681d7352def7c08fe018ecc32f9d241a3
Wyq1104 <ywei37@dons.usfca.edu>
2022-04-29 14:24:15 -0700

    emmitter 1.1

--- file/emmiter/emmiter.go
+++ file/emmiter/emmiter.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"file/messages"
-	"fmt"
 	"log"
 	"net"
 	"os"
@@ -21,7 +20,8 @@ func readFile(filename string) {
 	scanner := bufio.NewScanner(file)
 	// optionally, resize scanner's capacity for lines over 64K, see next example
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		// fmt.Println(scanner.Text())
+		parseLine(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -33,21 +33,21 @@ func readFile(filename string) {
 func parseLine(line string) {
 	words := strings.Fields(line)
 
-	timestamp, _ := strconv.Atoi(words[1])
+	timestamp, _ := strconv.ParseInt(words[1], 10, 64)
 	longitude, _ := strconv.ParseFloat(words[6], 32)
 	latitude, _ := strconv.ParseFloat(words[7], 32)
 	air_temperature, _ := strconv.ParseFloat(words[8], 32)
 	precipitaion, _ := strconv.ParseFloat(words[9], 32)
 	solar_radiation, _ := strconv.ParseFloat(words[10], 32)
 	surface_temperature, _ := strconv.ParseFloat(words[12], 32)
-	relative_humidity, _ := strconv.Atoi(words[15])
+	relative_humidity, _ := strconv.ParseInt(words[15], 10, 64)
 
 	sendData(timestamp, longitude, latitude, air_temperature, precipitaion, solar_radiation, surface_temperature, relative_humidity)
 
 }
 
-func sendData(timestamp int64, longitude float32, latitude float32, air_temperature float32, precipitaion float32, solar_radiation float32,
-	surface_temperature float32, relative_humidity int64) {
+func sendData(timestamp int64, longitude float64, latitude float64, air_temperature float64, precipitaion float64, solar_radiation float64,
+	surface_temperature float64, relative_humidity int64) {
 	nodename := "localhost"
 	conn, err := net.Dial("tcp", nodename+":20666")
 	if err != nil {
@@ -62,17 +62,8 @@ func sendData(timestamp int64, longitude float32, latitude float32, air_temperat
 	msgHandler.Send(wrapper)
 }
 
-// int64 UTC_TIMESTAMP = 1;
-//     float LONGITUDE = 2;
-//     float LATITUDE = 3;
-//     float AIR_TEMPERATURE = 4;
-//     float PRECIPITATION = 5;
-//     float SOLAR_RADIATION = 6;
-//     float SURFACE_TEMPERATURE = 7;
-//     int64 RELATIVE_HUMIDITY = 8;
-
 func main() {
 	// readFile("../../data/test.txt")
-	parseLine("63892 20180101 0005 20171231 1805      5  -88.14   32.84    -1.0     0.0 -99999 0 -9999.0 U 0 -9999 0 -99.000 -9999.0  1212 0 -99.00 0")
+	// parseLine("63892 20180101 0005 20171231 1805      5  -88.14   32.84    -1.0     0.0 -99999 0 -9999.0 U 0 -9999 0 -99.000 -9999.0  1212 0 -99.00 0")
 
 }
